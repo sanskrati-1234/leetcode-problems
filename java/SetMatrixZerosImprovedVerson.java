512ef114fd89f370774ee4f0e3e424e401d3cfa9
sanskrati-1234 <sanskratiagrawal306@gmail.com>
2022-01-22 22:10:35 +0530

    setmatrixzeroimprovedversion

---
+++ java/SetMatrixZerosImprovedVerson.java
@@ -0,0 +1,31 @@
+// tc =O(N^2)
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+      boolean[] row=new boolean[matrix.length];
+      boolean[] col=new boolean[matrix[0].length];
+      for(int i=0;i<matrix.length;i++){
+          for(int j=0;j<matrix[0].length;j++){
+              if(matrix[i][j]==0){
+                  row[i]=true;
+                  col[j]=true;
+              }
+          }
+      }
+      for(int i=0;i<matrix.length;i++){
+          if(row[i]==true){
+             for(int j=0;j<matrix[i].length;j++){
+                 matrix[i][j]=0;
+             }
+          }
+      }        
+      for(int i=0; i<matrix[0].length;i++){
+          if(col[i]==true){
+              for(int j=0;j<matrix.length;j++){
+                 matrix[j][i]=0;
+              }
+              
+          }
+      }  
+    }
+}
