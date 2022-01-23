b01276d82c1275c3280cb6ff7b8854bde79dd056
sanskrati-1234 <sanskratiagrawal306@gmail.com>
2022-01-23 13:33:05 +0530

    ugly-number

---
+++ java/UglyNumber.java
@@ -0,0 +1,18 @@
+/*
+An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+Given an integer n, return true if n is an ugly number.
+*/
+
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<1){
+            return false;
+        }
+        while(n%5==0) n=n/5;
+        while (n%3==0) n=n/3;
+        while (n%2==0) n=n/2;
+        return n==1;
+        
+    }
+}
